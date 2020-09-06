8eafa626478bd9eea50ba671bdd55dd155a49a5b
Italo Alves <contato.italo1020@gmail.com>
2020-09-06 16:48:52 -0300

    cria estrutura base da tela home e ajusta cartao de tarefas

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,19 @@ import React from "react";
 import ListCard from "../ListCard/ListCard";
 import Menu from "../Menu/Menu";
 
+import "./styles.css";
+
 const Home = () => (
   <div>
     <Menu />
-    <ListCard />
+    <div className="container-list">
+      <ListCard title="Ir ao médico" content="Fazer exames" />
+      <ListCard title="Fazer compras" content="comprar leite e pão" />
+      <ListCard title="Estudar" content="Estudar o projeto de AA" />
+      <ListCard title="Todo 3" content="content todo 3" />
+      <ListCard title="Todo 4" content="content todo 4" />
+      <ListCard title="Todo 5" content="content todo 5" />
+    </div>
   </div>
 );
 export default Home;
--- src/components/ListCard/ListCard.js
+++ src/components/ListCard/ListCard.js
@@ -3,16 +3,25 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-
+import IconButton from "@material-ui/core/IconButton";
+import faCheckCircle from "@fortawesome/free-solid-svg-icons/faCheckCircle";
+import Button from "@material-ui/core/Button";
 import "./styles.css";
 
 const ListCard = (props) => (
   <div className="card">
     <Card variant="outlined">
       <CardHeader>card header</CardHeader>
-      <Typography variant="outlined">Título</Typography>
+      <Typography className="title-todo" variant="outlined">
+        {props.title}
+      </Typography>
 
-      <CardContent>Título do todo</CardContent>
+      <CardContent className="content">Descrição: {props.content}</CardContent>
+      <CardContent>Data: {props.dateIniti}</CardContent>
+
+      <IconButton arial-label="Concluir tarefa">
+        <faCheckCircle></faCheckCircle>
+      </IconButton>
     </Card>
   </div>
 );
